18c76070a8cd7217e417153d84708b1d04fa9768
mostafa rastegar <m.rastegar991@gmail.com>
2021-12-24 22:33:40 +0330

    Start pandas package

---
+++ index.py
@@ -0,0 +1,37 @@
+# const_x = input('inter number: ')
+# if(int(const_x) > 2):
+#     print('hello')
+#     print('hello')
+# this is comment
+
+# x = "awesome"
+
+
+# def myfunc():
+#     x = "fantastic"
+#     print("Python is " + x)
+
+
+# myfunc()
+# print("Python is " + x)
+
+
+# x = "awesome"
+
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+
+
+# myfunc()
+
+# print("Python is " + x)
+
+
+import pandas as pd
+b = "Hello, World!"
+print(b[:2])
+
+housnig = pd.read_csv('./housing.csv')
+housnig.head()
